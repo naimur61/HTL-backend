2457c69427c09f9f8fd30acf98e821929f16f2fd
Naimur <naimur.r61n@gmail.com>
2023-10-24 00:33:21 +0600

    incomplete

--- app.ts
+++ app.ts
@@ -11,6 +11,12 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+interface TreeNode {
+	id: string;
+	label: string;
+	children?: TreeNode[];
+}
+
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.guif9pr.mongodb.net/your-database-name?retryWrites=true&w=majority`;
 
 async function run() {
@@ -31,7 +37,48 @@ async function run() {
 			res.status(200).send(result);
 		});
 
+		// app.get("/folders/:id", async (req: Request, res: Response) => {
+		// 	const targetId = req.params.id;
+		// 	const query = await folderCollection.find().toArray();
+		// 	const cursor = findNodeById(query);
+		// 	console.log(cursor);
+
+		// 	function findNodeById(query: TreeNode[]):boolean {
+		// 		query.map((e: TreeNode) => {
+		// 			if (e.id != targetId && e.children?.length !== 0) {
+		// 				findNodeById(e.children);
+		// 			} else {
+		// 				return true;
+		// 			}
+		// 		});
+		// 	}
+		// });
+
 		// Get Folder Collection
+
+		// app.get("/folders/:id", async (req: Request, res: Response) => {
+		// 	const targetId = req.params.id;
+		// 	const query = await folderCollection.find().toArray();
+		// 	const result = findNodeById(query);
+
+		// 	console.log(result); // Log the result
+
+		// 	function findNodeById(query: TreeNode[]): boolean {
+		// 		for (const node of query) {
+		// 			if (node.id !== targetId) {
+		// 				return true;
+		// 			}
+
+		// 			if (node.children && node.children.length > 0) {
+		// 				if (findNodeById(node.children)) {
+		// 					return true;
+		// 				}
+		// 			}
+		// 		}
+
+		// 		return false; // Node not found in this branch
+		// 	}
+		// });
 
 		/* <-------------------------------------------  X  -----------------------------------------> */
 		app.get("/", (req: Request, res: Response) => {
